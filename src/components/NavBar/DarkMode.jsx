c01c8f97785b657991c36bd92c001e1817a5d737
vmvvisakh <visakhvijayan798@gmail.com>
2024-09-24 12:54:14 +0530

    24/09

---
+++ src/components/NavBar/DarkMode.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import LightButton from "../../assets/website/light-mode-button.png";
+import DarkButton from "../../assets/website/dark-mode-button.png";
+
+const DarkMode = () => {
+    const [theme , setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light' )
+
+    const element = document.documentElement
+
+    useEffect(() => {
+        if (theme === "dark") {
+          element.classList.add("dark");
+          localStorage.setItem("theme", "dark");
+        } else {
+          element.classList.remove("dark");
+          localStorage.setItem("theme", "light");
+        }
+      }, [theme]);
+
+  return (
+    <div className='relative'>
+        <img src={LightButton}
+        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}        
+        className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${
+            theme === "dark" ? "opacity-0" : "opacity-100"
+          }`} alt="" />
+        <img src={DarkButton} onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+         className='w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 right-0 z-10' alt="" />
+    </div>
+  )
+}
+
+export default DarkMode
